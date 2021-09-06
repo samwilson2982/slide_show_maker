ef1810cd4016c18daf38e68c2a8111664e5bdf70
Samuel Wilson <samwilson2982@gmail.com>
2021-09-06 12:43:03 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.controllers import AppController
+#https://github.com/samwilson2982/slide_show_maker
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = AppController()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
